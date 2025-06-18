219fc1d20f0e7704f0eb227906f63a3feeac993b
ARTS-str <magninfacundo@gmail.com>
2025-06-17 21:30:57 -0300

    Agregar paradas

--- script.js
+++ script.js
@@ -24,38 +24,59 @@ function setup(){
     //POS x
     timelineX = new Timeline();
     //CENTROAMERICA
-    timelineX.addKeyframe(0.3, 150);
-    timelineX.addKeyframe(0.4, 150);
+    timelineX.addKeyframe(0.1, 180);
+    timelineX.addKeyframe(0.2, 180);
+    //LATAM
+    timelineX.addKeyframe(0.3, 250);
+    timelineX.addKeyframe(0.4, 250);
     //URUGUAY
-    timelineX.addKeyframe(0.5, 200);
-    timelineX.addKeyframe(0.6, 200);
-    timelineX.setAllValues(new Keyframe(0, 0), new Keyframe(1, 260));
+    timelineX.addKeyframe(0.5, 330);
+    timelineX.addKeyframe(0.6, 330);
+    //SIG PAG
+    timelineX.addKeyframe(0.7, 180);
+    timelineX.addKeyframe(0.8, 180);
+    timelineX.setAllValues(new Keyframe(0, 180), new Keyframe(1, 300));
 
     //POS Y
     timelineY = new Timeline();
     //CENTROAMERICA
-    timelineY.addKeyframe(0.3, 180);
-    timelineY.addKeyframe(0.4, 180);
+    timelineY.addKeyframe(0.1, 250);
+    timelineY.addKeyframe(0.2, 250);
+    //LATAM
+    timelineY.addKeyframe(0.3, 300);
+    timelineY.addKeyframe(0.4, 300);
     //URUGUAY
-    timelineY.addKeyframe(0.5, 300);
-    timelineY.addKeyframe(0.6, 300);
-    timelineY.setAllValues(new Keyframe(0, 0), new Keyframe(1, 368));
+    timelineY.addKeyframe(0.5, 450);
+    timelineY.addKeyframe(0.6, 450);
+    //SIG PAG
+    timelineY.addKeyframe(0.7, 550);
+    timelineY.addKeyframe(0.8, 550);
+    timelineY.setAllValues(new Keyframe(0, 0), new Keyframe(1, 850));
 
     //ZOOM
     timelineZ = new Timeline();
     //CENTROAMERICA
-    timelineZ.addKeyframe(0.3, 0.3);
-    timelineZ.addKeyframe(0.4, 0.3);
+    timelineZ.addKeyframe(0.1, 0.3);
+    timelineZ.addKeyframe(0.2, 0.3);
     //EFECTO ZOOM OUT
-    timelineZ.addKeyframe(0.45, 0.4);
+    timelineZ.addKeyframe(0.35, 0.5);
     //URUGUAY
+    timelineZ.addKeyframe(0.5, 0.1);
+    timelineZ.addKeyframe(0.6, 0.1);
+    //SIG PAG
+    timelineZ.addKeyframe(0.7, 0.5);
+    timelineZ.addKeyframe(0.8, 0.5);
 
-    timelineZ.setAllValues(new Keyframe(0, 1), new Keyframe(1, 0.06));
+
+    timelineZ.setAllValues(new Keyframe(0, 0.5), new Keyframe(1, 0.06));
 
     //PARADAS
     timelineS = new Timeline();
     timelineS.addKeyframe(0, 0);
-    timelineS.addKeyframe(0.3, 0);
+    timelineS.addKeyframe(0.1, 0);
+    timelineS.addKeyframe(0.35, 0);
+    timelineS.addKeyframe(0.55, 0);
+    timelineS.addKeyframe(0.75, 0);
     timelineS.addKeyframe(1, 0);
 }
 
